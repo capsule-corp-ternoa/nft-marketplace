26d3ff9b0e46655a8e4994e30c0e090f98f60e49
johann canu <johann.canu@axa.co.jp>
2021-01-23 23:22:46 +0900

    Add a server.js file to serve app in production and BE in dev phase (with proxy)

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+const nftApiUrl = `${process.env.NFT_API_HOST}/${process.env.NFT_API_PORT}`;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', (req, res) => res.send('pong'));
+
+// Get nfts info
+app.get('/nft-api/nfts', (req, res) => {
+  console.log(' >>>>>>> Entered in the backend side');
+  const options = {
+    url: `${nftApiUrl}/nfts`,
+    method: 'get',
+  };
+
+  return axios(options)
+    .then((response) => res.send(response.data));
+  
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(8181);

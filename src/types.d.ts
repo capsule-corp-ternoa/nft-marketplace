5c774c14b3198a665c916017f071fb00e22b1294
johann canu <johann.canu@axa.co.jp>
2021-01-23 23:24:24 +0900

    Gather all the type into one *.d.ts file

---
+++ src/types.d.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type ContextArgs = {
+  state: ContextState;
+  dispatch: React.Dispatch<ContextAction>;
+};
+
+type ContextState = {
+  isLoading: boolean;
+  nftList: any[];
+};
+
+type ContextAction = {
+  type: string;
+  payload: any;
+};
+
+type CategoryType = {
+  id: number;
+  name: string;
+  position: number;
+  logo?: string;
+};
+
+type NftListMockupType = {
+  labels: number[];
+  name: string;
+  quantity: string;
+  price: string;
+  image: string;
+};

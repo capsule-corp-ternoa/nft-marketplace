6db5f1f44aca5ea3c0bf8daaed73a0d891900773
johann canu <johann.canu@axa.co.jp>
2021-01-23 23:28:42 +0900

    Add utils.js file

---
+++ src/utils/utils.ts
@@ -0,0 +1,64 @@
+export const categoryList: CategoryType[] = [
+  {
+    id: 0,
+    name: 'art',
+    position: 1,
+  },
+  {
+    id: 1,
+    name: 'art',
+    position: 1,
+    logo: 'xxx.png',
+  },
+  {
+    id: 2,
+    name: 'game',
+    position: 2,
+    logo: 'yyy.png',
+  },
+];
+
+export const NftListMockup: NftListMockupType[] = [
+  {
+    labels: [1, 2, 3],
+    name: 'my NFT 1',
+    quantity: '10 of 10',
+    price: '0.2 ETH',
+    image: 'https://picsum.photos/200/300',
+  },
+  {
+    labels: [1, 2, 3],
+    name: 'my NFT 2',
+    quantity: '10 of 10',
+    price: '0.5 ETH',
+    image: 'https://picsum.photos/201/300',
+  },
+  {
+    labels: [1, 2, 3],
+    name: 'my NFT 3',
+    quantity: '10 of 10',
+    price: '0.01 ETH',
+    image: 'https://picsum.photos/202/300',
+  },
+  {
+    labels: [1, 2, 3],
+    name: 'my NFT 4',
+    quantity: '10 of 10',
+    price: '0.8 ETH',
+    image: 'https://picsum.photos/203/300',
+  },
+  {
+    labels: [1, 2, 3],
+    name: 'my NFT 5',
+    quantity: '10 of 10',
+    price: '1 ETH',
+    image: 'https://picsum.photos/204/300',
+  },
+  {
+    labels: [1, 2, 3],
+    name: 'my NFT 6',
+    quantity: '10 of 10',
+    price: '0.22 ETH',
+    image: 'https://picsum.photos/205/300',
+  },
+];

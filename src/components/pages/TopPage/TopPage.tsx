cafa0b491393dd80927e73bd248d84d6cae3d8f6
canujohann <canu.johann@gmail.com>
2021-02-05 23:05:08 +0900

    Add the gradient text element for top page

--- src/components/pages/TopPage/TopPage.tsx
+++ src/components/pages/TopPage/TopPage.tsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useContext } from 'react';
+import styled from 'styled-components';
 import { fetchNfts } from '../../../utils/store/dataFetcher';
 import { Context } from '../../../utils/store/store';
 import Carousel from './Carousel/Carousel';
 import { H1, H4 } from '../../common/Title/Title';
 import colors from '../../common/ui-library/styles/colors';
+
+const GradientText = styled.span`
+    background-color: #f3ec78;
+    background-image: ${colors.purple_gradient};
+    background-size: 100%;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -webkit-text-fill-color: transparent; 
+    -moz-text-fill-color: transparent;
+`;
 
 const TopPage: React.FC = () => {
 
@@ -19,9 +30,9 @@ const TopPage: React.FC = () => {
     <>
       <H1>
         Buy your &nbsp;
-        <span style={{ color: colors.purple }}>
+        <GradientText>
           collectible stamps &nbsp;
-        </span>
+        </GradientText>
         to  <br />
         send your ternoa capsules.
       </H1>

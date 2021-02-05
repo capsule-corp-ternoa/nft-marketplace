0a8c6e88eee3bdf0a9cfd4f0ca1f59fab8adc06f
canujohann <canu.johann@gmail.com>
2021-02-05 23:00:12 +0900

    Add a first sample test

---
+++ src/components/pages/ConnectWalletPage/ConnectWalletPage.test.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ConnectWalletPage from './ConnectWalletPage';
+
+// Known issue: error and warning does not work for console
+// https://github.com/facebook/react/issues/7047
+const original = console.error;
+
+beforeEach(() => {
+  console.error = jest.fn();
+});
+
+afterEach(() => {
+  console.error = original;
+});
+
+describe('ConnectWalletPage', () => {
+
+  it('renders component properly', () => {
+    render(<ConnectWalletPage />);
+    expect(screen.getByText(' Metamask')).toBeInTheDocument();
+    expect(screen.getByText(' Wallet Connect')).toBeInTheDocument();
+    expect(screen.getByText(' Fortmatic')).toBeInTheDocument();
+    expect(screen.getByText(' WalletLink')).toBeInTheDocument();
+  });
+
+});

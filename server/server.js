0a96ae3ed43e6b7a79b7ea6c054f3438138fbe1c
canujohann <canu.johann@gmail.com>
2021-02-19 11:59:13 +0900

    quick fix for heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const dotenv = require('dotenv');
+const mock = require('./mock');
 
 dotenv.config();
 
@@ -15,40 +16,50 @@ app.get('/ping', (req, res) => res.send('pong'));
 
 // Get nfts info
 app.get('/nft-api/nfts', (req, res) => {
-  console.log(' >>>>>>> Entered in the backend side');
-  const options = {
-    url: `${nftApiUrl}/nfts`,
-    method: 'get',
-  };
 
-  return axios(options)
-    .then((response) => res.send(response.data));
+  res.setHeader('Content-Type', 'application/json');
+  return res.end(JSON.stringify(mock.nfts));
+
+  // console.log(' >>>>>>> Entered in the backend side');
+  // const options = {
+  //   url: `${nftApiUrl}/nfts`,
+  //   method: 'get',
+  // };
+
+  // return axios(options)
+  //   .then((response) => res.send(response.data));
   
 });
 
 // Get nfts info
 app.get('/nft-api/nft/:id', (req, res) => {
-  console.log(' >>>>>>> Entered in the backend side');
-  const options = {
-    url: `${nftApiUrl}/nft/1`,
-    method: 'get',
-  };
+  // console.log(' >>>>>>> Entered in the backend side');
+  // const options = {
+  //   url: `${nftApiUrl}/nft/1`,
+  //   method: 'get',
+  // };
 
-  return axios(options)
-    .then((response) => res.send(response.data));
+  // return axios(options)
+  //   .then((response) => res.send(response.data));
+
+  res.setHeader('Content-Type', 'application/json');
+  return res.end(JSON.stringify(mock.nft));
   
 });
 
 // Get nfts info
 app.get('/nft-api/user/:id', (req, res) => {
-  console.log(' >>>>>>> Entered in the backend side');
-  const options = {
-    url: `${nftApiUrl}/user/1`,
-    method: 'get',
-  };
+  // console.log(' >>>>>>> Entered in the backend side');
+  // const options = {
+  //   url: `${nftApiUrl}/user/1`,
+  //   method: 'get',
+  // };
 
-  return axios(options)
-    .then((response) => res.send(response.data));
+  // return axios(options)
+  //   .then((response) => res.send(response.data));
+
+  res.setHeader('Content-Type', 'application/json');
+  return res.end(JSON.stringify(mock.user));
   
 });
 

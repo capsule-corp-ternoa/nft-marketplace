1674bde6fc2381e36be8b0545320087e52ef4f89
canujohann <canu.johann@gmail.com>
2021-02-19 22:30:28 +0900

    Remove mock server (directly put value in proxy server)

--- server/server.js
+++ server/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
 const dotenv = require('dotenv');
 const mock = require('./mock');
 
 dotenv.config();
-
 const app = express();
-
-const nftApiUrl = `${process.env.NFT_API_HOST}:${process.env.NFT_API_PORT}`;
 
 app.use(express.static(path.join(__dirname, '../build'), { index: false }));
 
@@ -16,56 +12,25 @@ app.get('/ping', (req, res) => res.send('pong'));
 
 // Get nfts info
 app.get('/nft-api/nfts', (req, res) => {
-
   res.setHeader('Content-Type', 'application/json');
   return res.end(JSON.stringify(mock.nfts));
-
-  // console.log(' >>>>>>> Entered in the backend side');
-  // const options = {
-  //   url: `${nftApiUrl}/nfts`,
-  //   method: 'get',
-  // };
-
-  // return axios(options)
-  //   .then((response) => res.send(response.data));
-  
 });
 
 // Get nfts info
 app.get('/nft-api/nft/:id', (req, res) => {
-  // console.log(' >>>>>>> Entered in the backend side');
-  // const options = {
-  //   url: `${nftApiUrl}/nft/1`,
-  //   method: 'get',
-  // };
-
-  // return axios(options)
-  //   .then((response) => res.send(response.data));
-
   res.setHeader('Content-Type', 'application/json');
   return res.end(JSON.stringify(mock.nft));
-  
 });
 
 // Get nfts info
 app.get('/nft-api/user/:id', (req, res) => {
-  // console.log(' >>>>>>> Entered in the backend side');
-  // const options = {
-  //   url: `${nftApiUrl}/user/1`,
-  //   method: 'get',
-  // };
-
-  // return axios(options)
-  //   .then((response) => res.send(response.data));
-
   res.setHeader('Content-Type', 'application/json');
   return res.end(JSON.stringify(mock.user));
-  
 });
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 8181);
 

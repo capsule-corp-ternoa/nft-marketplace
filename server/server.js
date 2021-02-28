3735a7ba028c61e57b52f62c3956c5b49fc49be2
canujohann <canu.johann@gmail.com>
2021-02-28 18:28:51 +0900

    Add a timeout to simulate API real behaviour

--- server/server.js
+++ server/server.js
@@ -6,24 +6,29 @@ const mock = require('./mock');
 dotenv.config();
 const app = express();
 
+const TIMEOUT = 1500;
+
 app.use(express.static(path.join(__dirname, '../build'), { index: false }));
 
 app.get('/ping', (req, res) => res.send('pong'));
 
 // Get nfts info
-app.get('/nft-api/nfts', (req, res) => {
+app.get('/nft-api/nfts', async (req, res) => {
+  await new Promise(resolve => setTimeout(resolve, TIMEOUT));
   res.setHeader('Content-Type', 'application/json');
   return res.end(JSON.stringify(mock.nfts));
 });
 
-// Get nfts info
-app.get('/nft-api/nft/:id', (req, res) => {
+// Get single nft info
+app.get('/nft-api/nft/:id', async (req, res) => {
+  await new Promise(resolve => setTimeout(resolve, TIMEOUT));
   res.setHeader('Content-Type', 'application/json');
   return res.end(JSON.stringify(mock.nft));
 });
 
-// Get nfts info
-app.get('/nft-api/user/:id', (req, res) => {
+// Get user info
+app.get('/nft-api/user/:id', async (req, res) => {
+  await new Promise(resolve => setTimeout(resolve, TIMEOUT));
   res.setHeader('Content-Type', 'application/json');
   return res.end(JSON.stringify(mock.user));
 });

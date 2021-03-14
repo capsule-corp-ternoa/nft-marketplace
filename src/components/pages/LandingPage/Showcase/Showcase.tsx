b7ca69f0a7a5b3457c5d80d6dd68592c49a29bbc
Cyril <cyril.caulonque@protonmail.com>
2021-03-14 22:32:06 +0100

    landing showcase carousel

--- src/components/pages/LandingPage/Showcase/Showcase.tsx
+++ src/components/pages/LandingPage/Showcase/Showcase.tsx
@@ -1,14 +1,32 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 import Switch from 'react-switch';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 import style from './Showcase.module.scss';
 
 import NFTCard from '../../../common/NftCard/NftCard';
 
-import 'react-multi-carousel/lib/styles.css';
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2,
+  }
+};
 
 const Showcase: React.FC<any> = ({ NFTs, category }) => {
   const [isFiltered, setIsFiltered] = useState(false);
+
+  let carousel : Carousel | null = null;
 
   function returnNFTs() {
     return NFTs.map((item: any) => <NFTCard key={item.id} item={item} />);
@@ -38,12 +56,22 @@ const Showcase: React.FC<any> = ({ NFTs, category }) => {
             </div>
           </div>
           <div className={style.Nav}>
-            <span>-</span>
-            <span>+</span>
+            <button type="button" onClick={() => carousel?.previous(1)}>-</button>
+            <button type="button" onClick={() => carousel?.next(1)}>+</button>
           </div>
         </div>
         <div className={style.Wrapper}>
-          <div className={style.NFTContainer}>{returnNFTs()}</div>
+          <div className={style.NFTContainer}>
+            <Carousel
+              ref={(el) => {carousel = el;}} 
+              responsive={responsive} 
+              infinite
+              arrows={false} 
+              centerMode
+            >
+              {returnNFTs()}
+            </Carousel>
+          </div>
         </div>
       </div>
     </>

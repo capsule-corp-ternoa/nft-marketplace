5e969c0ec1cf090de4126ea7022ea80ff0a68bec
PBQR <paul@mkdx.io>
2021-04-07 18:41:47 +0200

    hotfix wording alpha banner

--- components/base/AlphaBanner/AlphaBanner.tsx
+++ components/base/AlphaBanner/AlphaBanner.tsx
@@ -14,7 +14,15 @@ const AlphaBanner: React.FC = () => {
     <div className={style.Banner}>
       <p className={style.Text}>
         Welcome to the <span className={style.Alpha}> Alpha Version</span> of
-        SecretNFT. All the marketplace is in chaos net. Only FAKE CAPS are used.
+        SecretNFT. All the marketplace is in{' '}
+        <a
+          href="https://telemetry.polkadot.io/#list/Ternoa%20Chaos%20Net"
+          target="blank"
+          className={style.Link}
+        >
+          chaos net
+        </a>
+        .
       </p>
       <Link href="/faq">
         <a className={style.More}>More infos</a>

24391afe06ecc8e67223567a5562e65c1a309b0f
PBQR <paul@mkdx.io>
2021-04-20 13:18:22 +0200

    nft filter fix

--- actions/nft.tsx
+++ actions/nft.tsx
@@ -1,28 +1,34 @@
+import { NftType } from 'interfaces/index';
+
 export const getNFTS = async () => {
   const res = await fetch(
     'https://ternoa-marketplace-nft.herokuapp.com/api/NFTs'
   );
-  const data = await res.json();
+  let data: NftType[] = await res.json();
+
+  data = data.filter((item) => Number(item.id) < 26);
 
   return data;
 };
 
-export const getProfileNFTS = async (id: any) => {
+export const getProfileNFTS = async (id: string) => {
   const res = await fetch(
     `https://ternoa-marketplace-nft.herokuapp.com/api/NFTs/owner/${id}`
   );
 
-  const data = await res.json();
+  let data: NftType[] = await res.json();
+
+  data = data.filter((item) => Number(item.id) < 25);
 
   return data;
 };
 
-export const getNFT = async (id: any) => {
+export const getNFT = async (id: string) => {
   const res = await fetch(
     `https://ternoa-marketplace-nft.herokuapp.com/api/NFTs/${id}`
   );
 
-  if (!res.ok) throw new Error();
+  if (!res.ok || Number(id) > 25) throw new Error();
 
   const data = await res.json();
 

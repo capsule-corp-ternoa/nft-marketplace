ab70a99cb90773eb38faa8421a2a109de8ea7a4e
Maxime Lassalle <max@gama-smartweb.com>
2021-05-19 19:28:21 +0200

    socket RECEIVE_WALLET_ID event fix typo

--- components/base/TernoaWallet/TernoaWallet.tsx
+++ components/base/TernoaWallet/TernoaWallet.tsx
@@ -31,12 +31,14 @@ const TernoaWallet: React.FC<TernoaWalletProps> = ({ setModalExpand }) => {
     socket.on('disconnect', (data) => console.log('deco', data));
 
     socket.on('CONNECTION_FAILURE', (data) => setError(data.msg));
-    socket.on('SEND_WALLET_ID', (data) => {
+    socket.on('RECEIVE_WALLET_ID', (data) => {
+      console.log('SEND_WALLET_ID',data);      
       Cookies.set('token', data.walletId, {
         sameSite: 'strict',
         expires: 1,
       });
       socket.emit('RECEIVED_WALLET_ID', data);
+      socket.close();
       window.location.reload();
     });
 

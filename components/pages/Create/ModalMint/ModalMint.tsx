3ff36434cd0f2e8b4c0a94ef00a291af7aca3aaf
Maxime Lassalle <max@gama-smartweb.com>
2021-05-21 16:46:59 +0200

    Added UX for Mint / Buy processes interacting with mobile app

--- components/pages/Create/ModalMint/ModalMint.tsx
+++ components/pages/Create/ModalMint/ModalMint.tsx
@@ -6,6 +6,7 @@ import randomstring from 'randomstring';
 import io from 'socket.io-client';
 import QRCode from 'components/base/QRCode';
 import CheckMark from 'components/assets/checkmark';
+import { useRouter } from 'next/router'
 
 export interface ModalProps {
   setModalCreate: (b: boolean) => void;
@@ -26,6 +27,8 @@ const ModalMint: React.FC<ModalProps> = ({
   const [session] = useState(randomstring.generate());
   const [showQR, setShowQR] = useState(false);
   const [isRN, setIsRN] = useState(false);
+  const [mintReponse, setMintResponse] = useState(null)
+  const router = useRouter();
   const { walletId, price, links, fileHash } = QRData;
 
   useEffect(() => {
@@ -50,10 +53,14 @@ const ModalMint: React.FC<ModalProps> = ({
     });
 
     socket.on('CONNECTION_FAILURE', (data) => setError(data.msg));
-    socket.on('MINTING_NFT', (data) => {
-      console.log(data), setModalCreate(false);
+    socket.on('MINTING_NFT', ({ success }) => {
+      console.log('MINTING_NFT:' + success);
       socket.emit('MINTING_NFT_RECEIVED')
-      setModalCreate(false);
+      setMintResponse(success)
+      setTimeout(() => {
+        setModalCreate(false);
+        router.back()
+      }, 4000)
     });
     socket.on('disconnect', () => {
       setModalCreate(false);
@@ -70,24 +77,35 @@ const ModalMint: React.FC<ModalProps> = ({
     if (output.length > 0) {
       return (
         <>
-          <div className={style.Text}>
-            Flash this QR Code on your mobile wallet app to mint your NFT on the
-            Ternoa blockchain.
-          </div>
-          {showQR ? (
-            <div className={style.QR}>
-              <QRCode
-                data={{ session, walletId, price, links, fileHash }}
-                action={'MINT'}
-              />
+          { mintReponse === null && (
+            <>
+              <div className={style.Text}>
+                Flash this QR Code on your mobile wallet app to mint your NFT on the
+                Ternoa blockchain.
             </div>
-          ) : (
-              <div className={style.Loading}>
-                <span className={style.Dot}></span>
-                <span className={style.Dot}></span>
-                <span className={style.Dot}></span>
-              </div>
-            )}
+              {showQR && (
+                <div className={style.QR}>
+                  <QRCode
+                    data={{ session, walletId, price, links, fileHash }}
+                    action={'MINT'}
+                  />
+                </div>
+              )}
+              {!showQR && (
+                <div className={style.Loading}>
+                  <span className={style.Dot}></span>
+                  <span className={style.Dot}></span>
+                  <span className={style.Dot}></span>
+                </div>
+              )}
+            </>
+          )}
+          {(mintReponse === true) && <div className={style.Text}>
+            Mint was added to the blockchain.
+            </div>}
+          {(mintReponse === false) && <div className={style.Text}>
+            Mint was not added to the blockchain.
+            </div>}
         </>
       );
     } else {

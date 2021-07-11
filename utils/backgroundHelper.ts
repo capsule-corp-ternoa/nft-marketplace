55dbd3964113e4b7be62fd7fac47e5227c51408b
Leouarz <ghalielouarzazi@hotmail.com>
2021-07-11 18:43:44 +0200

    test background function endpoint

--- utils/backgroundHelper.ts
+++ utils/backgroundHelper.ts
@@ -1,11 +1,11 @@
 
 export async function fetchBackground(url: string){
     let res
-    // if (process.env.NODE_ENV === "development"){
-    //     res = await fetch(url)
+    // if (process.env.NODE_ENV !== "development" && process.env.BASE_URL!==undefined){
+    //     res = await fetch(process.env.BASE_URL+"/.netlify/functions/fetch-background?url="+encodeURIComponent(url))
     // }else{
-    //     res = await fetch("http://localhost:8888/.netlify/functions/fetch-background?url="+encodeURIComponent(url))
+    //     res = await fetch(url)
     // }
-    res = await fetch("http://localhost:8888/.netlify/functions/fetch?url="+encodeURIComponent(url))
+    res = await fetch(process.env.BASE_URL+"/.netlify/functions/fetch-background?url="+encodeURIComponent(url))
     return res
 }

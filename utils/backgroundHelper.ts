37b2b1a2e69af06990d14bf6ebfabd050b751524
Leouarz <ghalielouarzazi@hotmail.com>
2021-07-12 10:18:56 +0200

    update env var name

--- utils/backgroundHelper.ts
+++ utils/backgroundHelper.ts
@@ -1,11 +1,11 @@
 
 export async function fetchBackground(url: string){
     let res
-    // if (process.env.NODE_ENV !== "development" && process.env.BASE_URL!==undefined){
-    //     res = await fetch(process.env.BASE_URL+"/.netlify/functions/fetch-background?url="+encodeURIComponent(url))
+    // if (process.env.NODE_ENV !== "development" && process.env.NEXT_PUBLIC_BASE_URL!==undefined){
+    //     res = await fetch(process.env.NEXT_PUBLIC_BASE_URL+"/.netlify/functions/fetch-background?url="+encodeURIComponent(url))
     // }else{
     //     res = await fetch(url)
     // }
-    res = await fetch(process.env.BASE_URL+"/.netlify/functions/fetch-background?url="+encodeURIComponent(url))
+    res = await fetch(process.env.NEXT_PUBLIC_BASE_URL+"/.netlify/functions/fetch-background?url="+encodeURIComponent(url))
     return res
 }

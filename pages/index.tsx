734b5da42619d2a2136560bf4877b338e0e8d29f
Maxime Lassalle <max@gama-smartweb.com>
2021-08-02 11:25:31 +0200

    refactor to reach return on index function

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,28 +81,35 @@ export async function getServerSideProps(ctx: NextPageContext) {
   const token = cookies(ctx).token;
   // category code for beta testers NFTs
   const BETA_CODE = '001';
-  let [users = [], user = null, regularNfts = [], betaNfts = []] = await Promise.all([
-    getUsers(), token ? getUser(token) : Promise.resolve(null), getCategoryNFTs(), getCategoryNFTs(BETA_CODE)
-  ]).catch(e => {
-    console.error('Error retrieving data:' + e);
-    return [[], null, [], []]
-  });
-  users = arrayShuffle(users);
-  betaNfts = arrayShuffle(betaNfts || []).slice(0, 8);
-  let popularNfts = arrayShuffle((regularNfts || []).slice(0, 8));
-  let bestSellingNfts = arrayShuffle((regularNfts || []).slice(9, 17));
-  let NFTCreators = arrayShuffle((regularNfts || []).slice(18, 21));
-  let totalCountNFT = (regularNfts || []).length + (betaNfts || []).length;
-  return {
-    props: {
-      user,
-      users,
-      popularNfts,
-      bestSellingNfts,
-      NFTCreators,
-      betaNfts,
-      totalCountNFT,
-    },
+  let users: any[] = [], user = null, regularNfts: any[] = [], betaNfts: any[] = [];
+  try {
+    [users = [], user = null, regularNfts = [], betaNfts = []] = await Promise.all([
+      getUsers(), token ? getUser(token) : Promise.resolve(null), getCategoryNFTs(), getCategoryNFTs(BETA_CODE)
+    ]).catch(e => {
+      throw new Error(e);
+    });
+  }
+  catch (e) {
+    console.error('Error on index function:' + e);
+  }
+  finally {
+    users = arrayShuffle(users);
+    betaNfts = arrayShuffle(betaNfts || []).slice(0, 8);
+    let popularNfts = arrayShuffle((regularNfts || []).slice(0, 8));
+    let bestSellingNfts = arrayShuffle((regularNfts || []).slice(9, 17));
+    let NFTCreators = arrayShuffle((regularNfts || []).slice(18, 21));
+    let totalCountNFT = (regularNfts || []).length + (betaNfts || []).length;
+    return {
+      props: {
+        user,
+        users,
+        popularNfts,
+        bestSellingNfts,
+        NFTCreators,
+        betaNfts,
+        totalCountNFT,
+      },
+    }
   };
 }
 

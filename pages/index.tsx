ff3c7edc1a069ec03d8d4a2afc76bf7460c5669b
Leouarz <ghalielouarzazi@hotmail.com>
2021-09-02 16:32:02 +0200

    test cookie setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,10 +35,10 @@ const LandingPage: React.FC<LandingProps> = ({
 }) => {
   const [modalExpand, setModalExpand] = useState(false);
   const [notAvailable, setNotAvailable] = useState(false);
-  const [walletUser, setWalletUser] = useState(user);
+  //const [walletUser, setWalletUser] = useState(user);
 
   useEffect(() => {
-    async function callBack() {
+    /*async function callBack() {
       try {
         let res = await getUser(window.walletId);
         setWalletUser(res);
@@ -46,7 +46,7 @@ const LandingPage: React.FC<LandingProps> = ({
         console.error(error);
       }
     }
-    //if (window.isRNApp && window.walletId) callBack();
+    if (window.isRNApp && window.walletId) callBack();*/
     if (window.isRNApp && window.walletId && !Cookies.get('token')) Cookies.set('token', window.walletId, { expires: 1 });
   }, []);
 
@@ -62,11 +62,11 @@ const LandingPage: React.FC<LandingProps> = ({
       {modalExpand && <TernoaWallet setModalExpand={setModalExpand} />}
       {notAvailable && <NotAvailableModal setNotAvailable={setNotAvailable} />}
       <AlphaBanner />
-      <MainHeader user={walletUser} setModalExpand={setModalExpand} />
+      <MainHeader user={user} setModalExpand={setModalExpand} />
       <Landing
         setModalExpand={setModalExpand}
         setNotAvailable={setNotAvailable}
-        user={walletUser}
+        user={user}
         users={users}
         popularNfts={popularNfts}
         bestSellingNfts={bestSellingNfts}

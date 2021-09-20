9c3319c5ea087871ce1ef4ca77babd7662b02766
Leouarz <ghalielouarzazi@hotmail.com>
2021-09-20 21:15:07 +0200

    remove unused constants

--- utils/nftEncryption/ipfs.const.js
+++ utils/nftEncryption/ipfs.const.js
@@ -1,8 +1,5 @@
 export const ipfsNodes = {
-    ternoaPinataIpfsBaseUrl: `https://ternoa.mypinata.cloud`,
-    cloudfareIpfsBaseUrl: `https://cloudflare-ipfs.com`,
     ternoaIpfsBaseUrl: `https://ipfs.ternoa.dev`
-
 }
 export const ipfsBaseUrl = process.env.IPFS_BASEURL || ipfsNodes.ternoaIpfsBaseUrl;
 export const ipfsGatewayUri = `${ipfsBaseUrl}/ipfs`;

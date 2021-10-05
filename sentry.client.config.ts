d7da229d3e1c45efb8f50400a3cc17da539d31b7
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2021-10-05 13:51:06 +0200

    add check on env

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-if (SENTRY_DSN){
+if (SENTRY_DSN && process.env.SENTRY_AUTH_TOKEN){
   Sentry.init({
     dsn: SENTRY_DSN,
     environment: process.env.NODE_ENV,

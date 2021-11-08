085a2444ae24a8c74588e6e1b773fef547908109
Igor Papandinas <igor.papandinas@posteo.net>
2021-11-08 11:25:53 +0100

    feat: Styled layout components created

---
+++ components/base/Layout/Container.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Wrapper = styled.div`
+  width: 110rem;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 9.6rem 2.4rem;
+`;
---
+++ components/base/Layout/index.tsx
@@ -0,0 +1,9 @@
+export { Container, Wrapper } from './Container';
+export {
+  HiddenInput,
+  HiddenShell,
+  Input,
+  InputLabel,
+  InputShell,
+  Textarea,
+} from './Inputs';

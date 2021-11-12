7447ead0650520adaf67b4a4ba5192467dd4b1d9
Igor Papandinas <igor.papandinas@posteo.net>
2021-11-12 19:13:19 +0100

    feat: Dynamic blur with slider adjust

--- components/base/NftCard/NftCardWithEffects.tsx
+++ components/base/NftCard/NftCardWithEffects.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 import WhiteWaterMark from 'components/assets/WhiteWaterMark';
 import NftUpload from 'components/base/NftUpload';
@@ -11,6 +11,7 @@ import {
   NFT_FILE_TYPE_VIDEO,
 } from 'interfaces';
 import Chip from 'ui/components/Chip';
+import Slider from 'ui/components/Slider';
 
 interface Props {
   className?: string;
@@ -33,18 +34,21 @@ const DefaultEffect = css`
   position: absolute;
   transform: translateZ(0);
 `;
-const IMGDefaultEffect = styled.img`
+const IMGDefaultEffect = styled.img<{ blurredValue: string }>`
   ${DefaultEffect}
+  filter: ${({ blurredValue }) => `blur(${blurredValue}px)`};
+  -webkit-backdrop-filter: ${({ blurredValue }) => `blur(${blurredValue}px)`};
 `;
 const VideoDefaultEffect = styled.video`
   ${DefaultEffect}
 `;
 
-function returnType(NFTarg: File) {
+function returnType(NFTarg: File, blurredValue: string = '0') {
   if (NFTarg!.type.substr(0, 5) === NFT_FILE_TYPE_IMAGE) {
     return (
       <IMGDefaultEffect
         alt="img"
+        blurredValue={blurredValue}
         id="output"
         src={URL.createObjectURL(NFTarg)}
       />
@@ -72,64 +76,90 @@ const NftCardWithEffects = ({
   setError,
   setSecretNFT,
   setEffect,
-}: Props) => (
-  <MediaWrapper className={className}>
-    {returnType(NFT)}
-    {effect === NFT_EFFECT_BLUR && <Blur />}
-    {effect === NFT_EFFECT_PROTECT && (
-      <WaterMark>
-        <WaterMarkWrapper>
-          <WhiteWaterMarkIcon />
-        </WaterMarkWrapper>
-      </WaterMark>
-    )}
-    {effect === NFT_EFFECT_SECRET && (
-      <SecretWrapper>
-        {secretNFT === null ? (
-          <SecretUpload
-            content={
-              <SecretUploadDescription>
-                <SecretUploadTopDescription>
-                  Drag your the preview of your secret.
-                </SecretUploadTopDescription>
-                <span>
-                  Once purchased, the owner will be able to see your NFT
-                </span>
-              </SecretUploadDescription>
-            }
-            inputId="uploadSecretNft"
-            //isRN={isRN}
-            isSecretOption
-            note={`PNG, GIF, WEBP, MP4 or MP3. Max 30mb.`}
-            setError={setError}
-            setNFT={setSecretNFT}
-            setEffect={setEffect}
-          />
-        ) : (
-          <SecretMediaWrapper
-            content={returnType(secretNFT)}
-            inputId="reUploadSecretNft"
-            isMinimal
-            isSecretOption
-            setError={setError}
-            setNFT={setSecretNFT}
-            setEffect={setEffect}
-          />
-        )}
-        <SecretChip
-          color="transparent"
-          icon={<SecretChipIcon />}
-          text="Secret"
+}: Props) => {
+  const [blurredValue, setBlurredValue] = useState<string>('5');
+
+  const handleBlurredChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+    setEffect(NFT_EFFECT_BLUR);
+    setBlurredValue(target.value);
+  };
+
+  const handleBlurredClick = () => {
+    setEffect(NFT_EFFECT_BLUR);
+  };
+
+  return (
+    <MediaWrapper className={className}>
+      {returnType(NFT, effect === NFT_EFFECT_BLUR ? blurredValue : '0')}
+      {effect === NFT_EFFECT_BLUR && (
+        <SSlider
+          id="blurredSlider"
+          max="15"
+          min="0"
+          onChange={handleBlurredChange}
+          onClick={handleBlurredClick}
+          step={1}
+          value={blurredValue}
         />
-      </SecretWrapper>
-    )}
-  </MediaWrapper>
-);
+      )}
+      {effect === NFT_EFFECT_PROTECT && (
+        <WaterMark>
+          <WaterMarkWrapper>
+            <WhiteWaterMarkIcon />
+          </WaterMarkWrapper>
+        </WaterMark>
+      )}
+      {effect === NFT_EFFECT_SECRET && (
+        <SecretWrapper>
+          {secretNFT === null ? (
+            <SecretUpload
+              content={
+                <SecretUploadDescription>
+                  <SecretUploadTopDescription>
+                    Drag your the preview of your secret.
+                  </SecretUploadTopDescription>
+                  <span>
+                    Once purchased, the owner will be able to see your NFT
+                  </span>
+                </SecretUploadDescription>
+              }
+              inputId="uploadSecretNft"
+              //isRN={isRN}
+              isSecretOption
+              note={`PNG, GIF, WEBP, MP4 or MP3. Max 30mb.`}
+              setError={setError}
+              setNFT={setSecretNFT}
+              setEffect={setEffect}
+            />
+          ) : (
+            <SecretMediaWrapper
+              content={returnType(secretNFT)}
+              inputId="reUploadSecretNft"
+              isMinimal
+              isSecretOption
+              setError={setError}
+              setNFT={setSecretNFT}
+              setEffect={setEffect}
+            />
+          )}
+          <SecretChip
+            color="transparent"
+            icon={<SecretChipIcon />}
+            text="Secret"
+          />
+        </SecretWrapper>
+      )}
+    </MediaWrapper>
+  );
+};
 const MediaWrapper = styled.div`
   position: relative;
   width: 100%;
+  border-radius: 1.2rem;
   max-width: 250px;
   height: 430px;
+  overflow: hidden;
 `;
 
 const SecretMediaWrapper = styled(NftUpload)`
@@ -142,16 +172,12 @@ const SecretMediaWrapper = styled(NftUpload)`
   }
 `;
 
-const Blur = styled.div`
+const SSlider = styled(Slider)`
   width: 100%;
-  height: 100%;
   position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 3;
-  backdrop-filter: blur(8px);
-  -webkit-backdrop-filter: blur(8px);
-  border-radius: 1.2rem;
+  bottom: 4.8rem;
+  padding: 0 1.6rem;
+  z-index: 10;
 `;
 
 const WaterMark = styled.div`

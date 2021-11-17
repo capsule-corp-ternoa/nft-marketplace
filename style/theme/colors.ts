53514a7280683dffcb165ad499500a2221119bb4
Igor Papandinas <igor.papandinas@posteo.net>
2021-11-17 18:01:48 +0100

    fix: primaryLight color updated

--- style/theme/colors.ts
+++ style/theme/colors.ts
@@ -2,7 +2,7 @@ import { Colors } from './types'
 
 export const colors: Colors = {
   primary: '#7417ea',
-  primaryLight: 'rgba(116, 23, 234, 0.05)',
+  primaryLight: 'rgba(116, 23, 234, 0.1)',
   transparent: 'rgba(255, 255, 255, 0.4)',
 
   invertedContrast: '#fff',

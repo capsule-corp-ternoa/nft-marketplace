904537a5a996762e425bc2856096082c4bb99ad8
Igor Papandinas <igor.papandinas@posteo.net>
2021-11-17 12:14:51 +0100

    feat: Colors added to theme

--- style/theme/index.ts
+++ style/theme/index.ts
@@ -1,15 +1,18 @@
 import base from './base';
+import colors from './colors'
 import fonts from './fonts'
-import { Breakpoints, Fonts, MediaQueries } from './types';
+import { Breakpoints, Colors, Fonts, MediaQueries } from './types';
 
 export interface TernoaTheme {
   breakpoints: Breakpoints;
+  colors: Colors,
   fonts: Fonts,
   mediaQueries: MediaQueries;
 }
 
 const theme: TernoaTheme = {
   ...base,
+  ...colors,
   ...fonts,
 };
 

49c1f121eff8afa53f0d181b03989ccd5710cf2e
Igor Papandinas <igor.papandinas@posteo.net>
2021-11-17 17:59:42 +0100

    fix: Typography layout component updated

--- components/layout/Typography.tsx
+++ components/layout/Typography.tsx
@@ -16,6 +16,7 @@ export const Insight = styled.span`
 
 export const InsightLight = styled(Insight)`
   font-family: ${({ theme }) => theme.fonts.light};
+  font-size: 1rem;
 `;
 
 export const Subtitle = styled.h3`
@@ -29,10 +30,14 @@ export const Subtitle = styled.h3`
 `;
 
 export const Title = styled.h2`
+  display: inline-flex;
+  align-items: start;
   font-family: ${({ theme }) => theme.fonts.bold};
   font-size: 3.2rem;
+  gap: 2.4rem;
   line-height: 1.3;
   margin: 0;
+  max-width: 64rem;
   text-align: center;
 
   ${({ theme }) => theme.mediaQueries.md} {

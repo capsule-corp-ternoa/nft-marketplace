7c9daa89434f1336d7eed1433b603d62bd3a28f5
Igor Papandinas <igor.papandinas@posteo.net>
2021-11-18 19:18:30 +0100

    feat: Categories handled on NFT minting

--- components/pages/Create/Create.tsx
+++ components/pages/Create/Create.tsx
@@ -17,9 +17,11 @@ import NftPreview from 'components/base/NftPreview';
 import {
   NFT_EFFECT_DEFAULT,
   NFT_EFFECT_SECRET,
+  CategoryType,
   NftEffectType,
   UserType,
 } from 'interfaces';
+import Autocomplete from 'components/ui/Autocomplete';
 import Button from 'components/ui/Button';
 import Tooltip from 'components/ui/Tooltip';
 
@@ -32,6 +34,7 @@ type QRDataType = {
 };
 
 export interface CreateProps {
+  categoriesOptions: CategoryType[],
   NFT: File | null;
   NFTData: NFTProps;
   QRData: QRDataType;
@@ -48,6 +51,7 @@ export interface CreateProps {
 }
 
 const Create = ({
+  categoriesOptions,
   NFT,
   NFTData: initalValue,
   QRData,
@@ -66,7 +70,7 @@ const Create = ({
   const [isRN, setRN] = useState(false);
   const [nftData, setNFTData] = useState(initalValue);
   const [canAddToSeriesValue, setCanAddToSeriesValue] = useState(true)
-  const { description, name, quantity, seriesId } = nftData;
+  const { categories, description, name, quantity, seriesId } = nftData;
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -100,15 +104,36 @@ const Create = ({
     (effect !== NFT_EFFECT_SECRET || NFT) &&
     canAddToSeriesValue;
 
-  function onChange(
+  const handleChange = (
     e:
       | React.ChangeEvent<HTMLInputElement>
       | React.ChangeEvent<HTMLTextAreaElement>
-  ) {
+  ) => {
     const nextNftData = { ...nftData, [e.target.name]: e.target.value };
     setNFTData(nextNftData);
     setNftDataToParent(nextNftData);
-  }
+  };
+
+  const handleCategoryChipDelete = (
+    list: CategoryType[],
+    id: CategoryType['_id']
+  ) => {
+    const nextNftData = {
+      ...nftData,
+      categories: list.filter((item) => item._id !== id),
+    };
+    setNFTData(nextNftData);
+    setNftDataToParent(nextNftData);
+  };
+
+  const handleCategoryOptionClick = (option: CategoryType) => {
+    const nextNftData = {
+      ...nftData,
+      categories: categories.concat(option),
+    };
+    setNFTData(nextNftData);
+    setNftDataToParent(nextNftData);
+  };
 
   function initMintingNFT() {
     if (!user) throw new Error('Please login to create an NFT.');
@@ -161,7 +186,7 @@ const Create = ({
               <Input
                 type="text"
                 placeholder="Enter name"
-                onChange={onChange}
+                onChange={handleChange}
                 name="name"
                 value={name}
               />
@@ -173,25 +198,22 @@ const Create = ({
                 placeholder="Tell about the NFT in a few words..."
                 name="description"
                 value={description}
-                onChange={onChange}
+                onChange={handleChange}
               />
             </SInputShellDescription>
           </SLeft>
           <SRight>
-            {/* TODO in the future with autocomplete */}
-            {/* <InputShell>
-              <InputLabel>
-                Categories<SInsight>(optional)</SInsight>
-              </InputLabel>
-              <Input
-                type="text"
-                disabled
-                placeholder="NFT Category"
-                onChange={onChange}
-                name="category"
-                value={category}
-              />
-            </InputShell> */}
+            <Autocomplete<CategoryType>
+              label={
+                <>
+                  Categories<SInsight>(optional)</SInsight>
+                </>
+              }
+              list={categories}
+              onChipDelete={handleCategoryChipDelete}
+              onOptionClick={handleCategoryOptionClick}
+              options={categoriesOptions}
+            />
 
             {/* TODO in the future */}
             {/* <InputShell>
@@ -201,7 +223,7 @@ const Create = ({
               <Input
                 type="text"
                 placeholder="Enter royalties"
-                onChange={onChange}
+                onChange={handleChange}
                 name="royalties"
                 value={royalties}
               />
@@ -215,7 +237,7 @@ const Create = ({
                 type="text"
                 name="quantity"
                 value={quantity}
-                onChange={onChange}
+                onChange={handleChange}
                 placeholder="1"
                 isError={!validateQuantity(quantity, 10)}
               />
@@ -230,7 +252,7 @@ const Create = ({
               <Input
                 type="text"
                 placeholder="Enter ID"
-                onChange={onChange}
+                onChange={handleChange}
                 name="seriesId"
                 value={seriesId}
                 isError={!canAddToSeriesValue}

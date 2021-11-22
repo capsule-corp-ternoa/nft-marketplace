71d5c87f24fce4138b482030f221215b591b2a30
Igor Papandinas <igor.papandinas@posteo.net>
2021-11-22 15:55:12 +0100

    task: Formatting

--- utils/imageProcessing/image.ts
+++ utils/imageProcessing/image.ts
@@ -4,9 +4,9 @@ import { NftEffectType, NFT_EFFECT_BLUR, NFT_EFFECT_PROTECT } from 'interfaces';
 export async function imgToBlur(NFT: File | null, blurredValue: number) {
   try {
     let image = await Jimp.read(URL.createObjectURL(NFT));
-    let blurred = new Jimp(image.getWidth(), image.getHeight(), '#ffffff')
-    blurred.composite(image,0,0)
-    blurred.blur(blurredValue)
+    let blurred = new Jimp(image.getWidth(), image.getHeight(), '#ffffff');
+    blurred.composite(image, 0, 0);
+    blurred.blur(blurredValue);
     return await blurred.getBase64Async(image.getMIME());
   } catch (err) {
     console.log(err);
@@ -16,16 +16,17 @@ export async function imgToBlur(NFT: File | null, blurredValue: number) {
 export async function imgToWatermark(NFT: File | null) {
   const image = await Jimp.read(URL.createObjectURL(NFT));
   let watermark = await Jimp.read('/TernoaWatermark.png');
-  const imageWidth = image.getWidth()
-  const imageHeight = image.getHeight()
-  const bgColor = '#ffffff'
-  const protectedImage = new Jimp(imageWidth, imageHeight, bgColor)
-  const waterMarkSize = (imageWidth > imageHeight ? imageHeight : imageWidth)/5
-  const waterMarkMargin = waterMarkSize / 3.5
-  const wTruncated = 5/8 * imageHeight;
-  const hTruncated = 5/8 * imageWidth;
-  let xPos = 0
-  let yPos = 0
+  const imageWidth = image.getWidth();
+  const imageHeight = image.getHeight();
+  const bgColor = '#ffffff';
+  const protectedImage = new Jimp(imageWidth, imageHeight, bgColor);
+  const waterMarkSize =
+    (imageWidth > imageHeight ? imageHeight : imageWidth) / 5;
+  const waterMarkMargin = waterMarkSize / 3.5;
+  const wTruncated = (5 / 8) * imageHeight;
+  const hTruncated = (5 / 8) * imageWidth;
+  let xPos = 0;
+  let yPos = 0;
   watermark = watermark.resize(waterMarkSize, waterMarkSize);
   if (imageWidth > imageHeight) {
     xPos = imageWidth / 2 + wTruncated / 2 - (waterMarkMargin + waterMarkSize);
@@ -34,7 +35,7 @@ export async function imgToWatermark(NFT: File | null) {
     xPos = imageWidth - (waterMarkSize + waterMarkMargin);
     yPos = imageHeight / 2 + hTruncated / 2 - (waterMarkMargin + waterMarkSize);
   }
-  protectedImage.composite(image,0,0)
+  protectedImage.composite(image, 0, 0);
   protectedImage.composite(watermark, xPos, yPos);
   return await protectedImage.getBase64Async(image.getMIME());
 }
@@ -43,7 +44,7 @@ export const processFile = async (
   NFT: File,
   effect: NftEffectType,
   setError: (error: string) => void,
-  blurredValue?: number,
+  blurredValue?: number
 ) => {
   try {
     const newProcessedFile = new File([NFT], 'NFT', {

9aa478a23e262125af6652d4d99c5564fb87331c
Igor Papandinas <igor.papandinas@posteo.net>
2021-11-23 13:24:49 +0100

    fix: Dynamic position for the protect mark

--- utils/imageProcessing/image.ts
+++ utils/imageProcessing/image.ts
@@ -1,6 +1,8 @@
 import Jimp from 'jimp';
 import { NftEffectType, NFT_EFFECT_BLUR, NFT_EFFECT_PROTECT } from 'interfaces';
 
+const NFT_CARD_SIZE_RATIO = 0.625;
+
 export async function imgToBlur(NFT: File | null, blurredValue: number) {
   try {
     let image = await Jimp.read(URL.createObjectURL(NFT));
@@ -18,23 +20,27 @@ export async function imgToWatermark(NFT: File | null) {
   let watermark = await Jimp.read('/TernoaWatermark.png');
   const imageWidth = image.getWidth();
   const imageHeight = image.getHeight();
+  const ratio = imageWidth / imageHeight;
+  const coeff = (ratio > 1 ? 1 / ratio : ratio) * NFT_CARD_SIZE_RATIO;
   const bgColor = '#ffffff';
   const protectedImage = new Jimp(imageWidth, imageHeight, bgColor);
-  const waterMarkSize =
-    (imageWidth > imageHeight ? imageHeight : imageWidth) / 5;
-  const waterMarkMargin = waterMarkSize / 3.5;
-  const wTruncated = (5 / 8) * imageHeight;
-  const hTruncated = (5 / 8) * imageWidth;
-  let xPos = 0;
-  let yPos = 0;
+  const waterMarkSize = (ratio > 1 ? imageWidth / ratio : imageHeight) / 5;
+  const waterMarkMargin = waterMarkSize * NFT_CARD_SIZE_RATIO;
+  const waterMarkMarginFromCenter = (ratio > 1 ? imageWidth : imageHeight) / 4;
+
   watermark = watermark.resize(waterMarkSize, waterMarkSize);
-  if (imageWidth > imageHeight) {
-    xPos = imageWidth / 2 + wTruncated / 2 - (waterMarkMargin + waterMarkSize);
-    yPos = imageHeight - (waterMarkSize + waterMarkMargin);
+
+  const xPos =
+    imageWidth / 2 +
+    ((waterMarkMarginFromCenter - waterMarkMargin) * coeff) / 2;
+
+  let yPos = imageHeight / 2;
+  if (ratio > 1) {
+    yPos = yPos + waterMarkMarginFromCenter * coeff;
   } else {
-    xPos = imageWidth - (waterMarkSize + waterMarkMargin);
-    yPos = imageHeight / 2 + hTruncated / 2 - (waterMarkMargin + waterMarkSize);
+    yPos = yPos + waterMarkMarginFromCenter * coeff * 2;
   }
+
   protectedImage.composite(image, 0, 0);
   protectedImage.composite(watermark, xPos, yPos);
   return await protectedImage.getBase64Async(image.getMIME());

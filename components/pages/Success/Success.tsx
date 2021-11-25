13e07d49174f710d6b1cdcc3d27e9f3bca5476a1
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2021-11-25 15:26:43 +0100

    add key in div map in success page

--- components/pages/Success/Success.tsx
+++ components/pages/Success/Success.tsx
@@ -33,7 +33,7 @@ const Success = ({
         <SuccessImage className={style.Image}/>
         <h1 className={style.Title}>{title}</h1>
         {text && <div className={style.Text}>{text}</div>}
-        {subText && subText.split(',').filter(x => x.length > 0).map(x => <div className={style.SubText}>{x}</div>)}
+        {subText && subText.split(',').filter(x => x.length > 0).map((x,i) => <div className={style.SubText} key={i}>{x}</div>)}
         <Link href={returnUrl}>
           <a className={style.Button}>{buttonText}</a>
         </Link>

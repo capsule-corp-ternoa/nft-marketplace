562c9c47482e1eb4c22de4e6d1c38eaa2cbe9721
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2021-11-28 17:03:55 +0100

    add correct explorer endpoint

--- utils/constant.ts
+++ utils/constant.ts
@@ -4,4 +4,4 @@ export const DEFAULT_LIMIT_PAGINATION = "10"
 export const MARKETPLACE_ID = process.env.NEXT_PUBLIC_MARKETPLACE_ID || "0"
 export const SOCKET_URL = (process.env.NEXT_PUBLIC_SOCKET_API && removeURLSlash(process.env.NEXT_PUBLIC_SOCKET_API)) || (process.env.NEXT_PUBLIC_NODE_API && removeURLSlash(process.env.NEXT_PUBLIC_NODE_API)) || ""
 export const NODE_API_URL = process.env.NEXT_PUBLIC_NODE_API && removeURLSlash(process.env.NEXT_PUBLIC_NODE_API)
-export const EXPLORER_URL = process.env.NEXT_PUBLIC_EXPLORER_URL ? removeURLSlash(process.env.NEXT_PUBLIC_EXPLORER_URL) : "https://explorer-git-develop-ternoa.vercel.app"
+export const EXPLORER_URL = process.env.NEXT_PUBLIC_EXPLORER_URL ? removeURLSlash(process.env.NEXT_PUBLIC_EXPLORER_URL) : "https://explorer.ternoa.com/"

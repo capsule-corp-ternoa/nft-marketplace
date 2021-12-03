039cefff752f09182dedc30028f6df7b83229b39
Igor Papandinas <igor.papandinas@posteo.net>
2021-12-03 11:44:14 +0100

    fix: Layout container width marked out

--- components/layout/Container.tsx
+++ components/layout/Container.tsx
@@ -1,9 +1,11 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { breakpointMap } from 'style/theme/base';
 
 export const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  margin: 0 auto;
 `;
 
 export const Wrapper = styled.div`
@@ -14,11 +16,17 @@ export const Wrapper = styled.div`
   padding: 3.2rem 2.4rem;
 
   ${({ theme }) => theme.mediaQueries.md} {
-    padding: 6.4rem 2.4rem;
+    padding: 6.4rem 3.2rem;
   }
 
   ${({ theme }) => theme.mediaQueries.xl} {
-    max-width: 112rem;
-    padding: 9.6rem 2.4rem;
+    padding: 9.6rem 4.8rem;
+  }
+
+  ${({ theme }) => theme.mediaQueries.xxl} {
+    max-width: 1440px;
+    // TODO: Use xxl breakpoint when its value is 1440px
+    // max-width: ${breakpointMap.xxl}px;
+    padding: 9.6rem;
   }
 `;

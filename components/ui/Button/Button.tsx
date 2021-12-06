8ba8a3444d4b286ec2f63b4e0d0e307d8cd1d0d4
Igor Papandinas <igor.papandinas@posteo.net>
2021-12-06 23:35:51 +0100

    feat: Size/color updated on Button ui component

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -4,6 +4,7 @@ import { Colors } from 'style/theme/types';
 
 interface IButton {
   color?: keyof Colors;
+  size?: 'small' | 'medium';
 }
 interface Props extends IButton {
   className?: string;
@@ -22,11 +23,11 @@ const ButtonStyle = css<IButton>`
     color ? theme.colors[`${color}`] : theme.colors.primary};
   border: none;
   border-radius: 4rem;
-  color: white;
   cursor: pointer;
   font-family: ${({ theme }) => theme.fonts.bold};
   font-size: 1.6rem;
-  padding: 1.2rem 4.8rem;
+  padding: ${({ size }) =>
+    size === 'small' ? '0.8rem 1.6rem' : '1.2rem 4.8rem'};
   transition: all 0.6s cubic-bezier(0.25, 1, 0.5, 1);
   z-index: 1;
 
@@ -39,12 +40,33 @@ const ButtonStyle = css<IButton>`
     opacity: 0.4;
     pointer-events: none;
   }
+
+  color: ${({ theme, color }) => {
+    switch (color) {
+      case 'primary':
+        return theme.colors.invertedContrast;
+      case 'primaryLight':
+        return theme.colors.primary;
+      case 'invertedContrast':
+      case 'whiteBlur':
+      default:
+        return theme.colors.contrast;
+    }
+  }};
 `;
 
-const Button = ({ className, color, disabled, href, onClick, text }: Props) => {
+const Button = ({
+  className,
+  color,
+  disabled,
+  href,
+  onClick,
+  size = 'medium',
+  text,
+}: Props) => {
   if (href !== null && href !== undefined) {
     return (
-      <SAnchor className={className} color={color} href={href}>
+      <SAnchor className={className} color={color} href={href} size={size}>
         {text}
       </SAnchor>
     );
@@ -56,6 +78,7 @@ const Button = ({ className, color, disabled, href, onClick, text }: Props) => {
       color={color}
       disabled={disabled}
       onClick={onClick}
+      size={size}
     >
       {text}
     </SButton>

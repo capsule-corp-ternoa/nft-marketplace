0a937cdeafa18c8f048b156b6c2585fc65ec8f0d
Igor Papandinas <igor.papandinas@posteo.net>
2021-12-15 22:49:47 +0100

    fix: File is resized before processing

--- utils/imageProcessing/image.ts
+++ utils/imageProcessing/image.ts
@@ -1,83 +1,67 @@
 import Jimp from 'jimp';
 import { NftEffectType, NFT_EFFECT_BLUR, NFT_EFFECT_PROTECT } from 'interfaces';
 
-const NFT_CARD_SIZE_RATIO = 0.625;
-
-export async function imgToBlur(NFT: File | null, blurredValue: number) {
+export async function imgToBlur(image: Jimp, blurredValue: number) {
   try {
-    if (NFT){
-      let image = await Jimp.read(URL.createObjectURL(NFT));
-      let blurred = new Jimp(image.getWidth(), image.getHeight(), '#ffffff');
-      blurred.composite(image, 0, 0);
-      blurred.blur(blurredValue);
-      return await blurred.getBase64Async(image.getMIME());
-    }
+    let blurred = new Jimp(image.getWidth(), image.getHeight(), '#ffffff');
+    blurred.composite(image, 0, 0);
+    blurred.blur(blurredValue);
+    return await blurred.getBase64Async(image.getMIME());
   } catch (err) {
     console.log(err);
   }
 }
 
-export async function imgToWatermark(NFT: File | null) {
-  if (NFT){
-    const image = await Jimp.read(URL.createObjectURL(NFT));
-    let watermark = await Jimp.read('/TernoaWatermark.png');
-    const imageWidth = image.getWidth();
-    const imageHeight = image.getHeight();
-    const ratio = imageWidth / imageHeight;
-    const coeff = (ratio > 1 ? 1 / ratio : ratio) * NFT_CARD_SIZE_RATIO;
-    const bgColor = '#ffffff';
-    const protectedImage = new Jimp(imageWidth, imageHeight, bgColor);
-    const waterMarkSize =
-      ((ratio > 1 ? imageWidth / ratio : imageHeight) * NFT_CARD_SIZE_RATIO) / 4;
-    const waterMarkMargin = waterMarkSize * NFT_CARD_SIZE_RATIO;
-    const waterMarkMarginFromCenter = (ratio > 1 ? imageWidth : imageHeight) / 4;
-  
-    watermark = watermark.resize(waterMarkSize, waterMarkSize);
-    if (ratio < 0.4) {
-      watermark = watermark.resize(waterMarkSize / 2, waterMarkSize / 2);
-    }
-  
-    const xPos =
-      imageWidth / 2 +
-      ((waterMarkMarginFromCenter - waterMarkMargin) * coeff) / 2;
-  
-    let yPos = imageHeight / 2;
-    if (ratio > 1) {
-      yPos = yPos + waterMarkMarginFromCenter * coeff;
-    } else {
-      yPos = yPos + (waterMarkMarginFromCenter - waterMarkMargin) * coeff * 2;
-    }
-  
-    protectedImage.composite(image, 0, 0);
-    protectedImage.composite(watermark, xPos, yPos);
-    return await protectedImage.getBase64Async(image.getMIME());
-  }
+export async function imgToWatermark(image: Jimp) {
+  const imageWidth = image.getWidth();
+  const imageHeight = image.getHeight();
+  const ratio = imageWidth / imageHeight;
+
+  let watermark = await Jimp.read('/TernoaWatermark.png');
+  const watermarkSize = Math.min(
+    (ratio > 1 ? imageHeight : imageWidth) / 4,
+    90
+  );
+  const margin = watermarkSize * 0.2;
+
+  watermark = watermark.resize(watermarkSize, watermarkSize);
+
+  let xPos = imageWidth / 2 + margin;
+  let yPos = imageHeight / 2 + watermarkSize - margin;
+
+  const protectedImage = new Jimp(imageWidth, imageHeight, '#ffffff');
+  protectedImage.composite(image, 0, 0);
+  protectedImage.composite(watermark, xPos, yPos);
+  return await protectedImage.getBase64Async(image.getMIME());
 }
 
 export const processFile = async (
-  NFT: File,
+  rawFile: File,
   effect: NftEffectType,
   setError: (error: string) => void,
   blurredValue?: number
 ) => {
   try {
-    const newProcessedFile = new File([NFT], 'NFT', {
-      type: NFT.type,
+    const file = new File([rawFile], 'NFT', {
+      type: rawFile.type,
     });
+
     let res;
+    let image = await Jimp.read(URL.createObjectURL(file));
+    image = image.resize(Jimp.AUTO, 400);
 
     if (effect === NFT_EFFECT_BLUR && blurredValue) {
-      res = await imgToBlur(newProcessedFile, blurredValue);
+      res = await imgToBlur(image, blurredValue);
     } else if (effect === NFT_EFFECT_PROTECT) {
-      res = await imgToWatermark(newProcessedFile);
+      res = await imgToWatermark(image);
     }
 
     const blob = await (await fetch(res as string)).blob();
-    const file = new File([blob], NFT.name, {
-      type: NFT.type,
+    const newprocessedFile = new File([blob], rawFile.name, {
+      type: rawFile.type,
     });
 
-    return await file;
+    return await newprocessedFile;
   } catch (err) {
     setError('Please try again.');
     console.log(err);

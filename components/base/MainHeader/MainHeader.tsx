e54d9019e440a297531f712a38f48623174b1f4b
Igor Papandinas <igor.papandinas@posteo.net>
2022-01-06 15:43:20 +0100

    fix(MainHeader): Layout based on lg breakpoint

--- components/base/MainHeader/MainHeader.tsx
+++ components/base/MainHeader/MainHeader.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import Link from 'next/link';
 import styled from 'styled-components';
 
 import Logo from 'components/assets/LogoTernoaBlack';
 import { ProfileMenuBadge, ProfileMenuDropdown } from 'components/base/ProfileMenu';
 import Button from 'components/ui/Button';
+import { Container, Wrapper } from 'components/layout';
 
 import { UserType } from 'interfaces/index';
 import { computeCaps } from 'utils/strings';
+import { breakpointMap } from 'style/theme/base';
 
 import style from './MainHeader.module.scss';
 
@@ -17,84 +20,107 @@ export interface HeaderProps {
 }
 
 const MainHeader: React.FC<HeaderProps> = ({ setModalExpand, user }) => {
-  const [, setSearchValue] = useState('' as string);
   const [isProfileMenuExpanded, setIsProfileMenuExpanded] = useState(false);
-  const isNftCreationEnabled = process.env.NEXT_PUBLIC_IS_NFT_CREATION_ENABLED===undefined ? true : process.env.NEXT_PUBLIC_IS_NFT_CREATION_ENABLED === 'true'
-  const updateKeywordSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(event.currentTarget.value);
-  };
+  const isNftCreationEnabled =
+    process.env.NEXT_PUBLIC_IS_NFT_CREATION_ENABLED === undefined
+      ? true
+      : process.env.NEXT_PUBLIC_IS_NFT_CREATION_ENABLED === 'true';
+
+  const isMobileTablet = useMediaQuery({
+    query: `(max-width: ${breakpointMap.lg - 1}px)`,
+  });
 
   return (
-    <div className={style.Header}>
-      <div className={style.HeaderContainer}>
+    <Container>
+      <SWrapper>
         <Link href="/">
           <a>
             <Logo className={style.Logo} onClick={() => true} />
           </a>
         </Link>
-        <div className={style.SearchBar}>
-          <input
-            type="search"
-            onChange={updateKeywordSearch}
-            className={style.Input}
-            placeholder="Search"
-          />
-        </div>
-        <div className={style.Infos}>
-          <div className={style.Links}>
-            <Link href="/explore">
-              <a className={style.LinkItem}>Explore</a>
-            </Link>
-            <Link href="/faq">
-              <a className={style.LinkItem}>How it works</a>
-            </Link>
-          </div>
-          <SNavButtonsCointainer>
-            {isNftCreationEnabled && (
-              <Link href="/create" passHref>
-                <>
-                  <Button
-                    color="invertedContrast"
-                    href="/create"
-                    size="medium"
-                    text="Create NFT"
-                    variant="outlined"
-                  />
-                </>
+        {!isMobileTablet && (
+          <SNavContainer>
+            <SNavLinksContainer>
+              <Link href="/explore" passHref>
+                <SLinkItem>Explore</SLinkItem>
               </Link>
-            )}
-            {user ? (
-              <ProfileMenuBadge
-                onClick={() =>
-                  setIsProfileMenuExpanded((prevState) => !prevState)
-                }
-                tokenAmount={
-                  user?.capsAmount ? computeCaps(Number(user.capsAmount)) : 0
-                }
-                tokenSymbol="CAPS"
-                user={user}
-              />
-            ) : (
-              <Button
-                color="contrast"
-                onClick={() => setModalExpand(true)}
-                size="medium"
-                text="Connect"
-                variant="outlined"
-              />
-            )}
-          </SNavButtonsCointainer>
-        </div>
-        {user && isProfileMenuExpanded && (
-          <SProfileMenuDropdown
-            onClose={() => setIsProfileMenuExpanded(false)}
-            user={user}
-          />
+              <Link href="/faq" >
+                <SLinkItem>How it works</SLinkItem>
+              </Link>
+            </SNavLinksContainer>
+            <SNavButtonsCointainer>
+              {isNftCreationEnabled && (
+                <Link href="/create" passHref>
+                  <>
+                    <Button
+                      color="invertedContrast"
+                      href="/create"
+                      size="medium"
+                      text="Create NFT"
+                      variant="outlined"
+                    />
+                  </>
+                </Link>
+              )}
+              {user ? (
+                <ProfileMenuBadge
+                  onClick={() => setIsProfileMenuExpanded((prevState) => !prevState)}
+                  tokenAmount={user?.capsAmount ? computeCaps(Number(user.capsAmount)) : 0}
+                  tokenSymbol="CAPS"
+                  user={user}
+                />
+              ) : (
+                <Button
+                  color="contrast"
+                  onClick={() => setModalExpand(true)}
+                  size="medium"
+                  text="Connect"
+                  variant="outlined"
+                />
+              )}
+            </SNavButtonsCointainer>
+          </SNavContainer>
         )}
-      </div>
-    </div>
+        {user && isProfileMenuExpanded && (
+          <SProfileMenuDropdown onClose={() => setIsProfileMenuExpanded(false)} user={user} />
+        )}
+      </SWrapper>
+    </Container>
   );
 };
+
+const SWrapper = styled(Wrapper)`
+  flex-direction: row;
+  justify-content: center;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    justify-content: space-between;
+  }
+`;
+
+const SNavContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SNavLinksContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  > * {
+    &:not(:first-child) {
+      margin-left: 2.4rem;
+    }
+  }
+`;
+
+const SLinkItem = styled.a`
+  color: ${({theme}) => theme.colors.contrast};
+  cursor: pointer;
+  font-family: ${({theme}) => theme.fonts.bold};
+  font-size: 1.6rem;
+`;
+
 
 const SNavButtonsCointainer = styled.div`
   display: flex;
@@ -102,7 +128,7 @@ const SNavButtonsCointainer = styled.div`
   justify-content: center;
   gap: 2.4rem;
 
-  ${({ theme }) => theme.mediaQueries.xxl} {
+  ${({ theme }) => theme.mediaQueries.lg} {
     margin-left: 5.6rem;
   }
 `;
@@ -117,8 +143,7 @@ const SProfileMenuDropdown = styled(ProfileMenuDropdown)`
     border-left: 2.4rem solid transparent;
     border-right: 2.4rem solid transparent;
     z-index: 101;
-    border-top: ${({ theme }) =>
-      `1.2rem solid ${theme.colors.invertedContrast}`};
+    border-top: ${({ theme }) => `1.2rem solid ${theme.colors.invertedContrast}`};
     content: '';
     position: absolute;
     top: -1.6rem;

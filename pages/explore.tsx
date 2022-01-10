00b2b149e4c64625920a21ab977514b391fa1dec
Igor Papandinas <igor.papandinas@posteo.net>
2022-01-10 10:28:23 +0100

    fix: Default pagination limit increased to 12

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -113,7 +113,7 @@ export async function getServerSideProps(ctx: NextPageContext) {
   }
   promises.push(
     new Promise<void>((success) => {
-      getNFTs(undefined, undefined, "12", true, true)
+      getNFTs(undefined, undefined, undefined, true, true)
         .then((result) => {
           data = result.data;
           dataHasNextPage = result.hasNextPage || false;

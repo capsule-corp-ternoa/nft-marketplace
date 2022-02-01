54a0f24ff77d360dbf043bb80fc731265ef6bb66
Igor Papandinas <igor.papandinas@posteo.net>
2022-02-01 17:39:08 +0100

    task: Useless action types in redux removed

--- redux/app/types.ts
+++ redux/app/types.ts
@@ -8,16 +8,3 @@ export interface AppState {
 // Actions names
 export const APP_SET_IS_RN = 'APP_SET_IS_RN';
 export const APP_SET_USER = 'APP_SET_USER';
-
-// Actions interfaces
-interface SetIsRN {
-  type: typeof APP_SET_IS_RN;
-  value: boolean;
-}
-
-interface SetUser {
-  type: typeof APP_SET_USER;
-  value: UserType;
-}
-
-export type AppActionTypes = SetIsRN | SetUser;
--- redux/marketplaceData/types.ts
+++ redux/marketplaceData/types.ts
@@ -13,26 +13,3 @@ export const MP_SET_INSTAGRAM_URL = 'MP_SET_INSTAGRAM_URL';
 export const MP_SET_TWITTER_URL = 'MP_SET_TWITTER_URL';
 export const MP_SET_URL = 'MP_SET_URL';
 
-// Actions interfaces
-interface SetLogo {
-  type: typeof MP_SET_LOGO;
-  value: string;
-}
-interface SetName {
-  type: typeof MP_SET_NAME;
-  value: string;
-}
-interface SetInstagramUrl {
-  type: typeof MP_SET_INSTAGRAM_URL;
-  value: string;
-}
-interface SetTwitterUrl {
-  type: typeof MP_SET_TWITTER_URL;
-  value: string;
-}
-interface SetUrl {
-  type: typeof MP_SET_URL;
-  value: string;
-}
-
-export type MpActionTypes = SetLogo | SetName | SetInstagramUrl | SetTwitterUrl | SetUrl;

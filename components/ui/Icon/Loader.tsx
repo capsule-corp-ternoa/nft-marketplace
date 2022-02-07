fbec00d27f8c71ee4c1dcdd36fbda2f82b99c25e
Igor Papandinas <igor.papandinas@posteo.net>
2022-02-07 12:55:31 +0100

    fix(Loader): Useless margin removed

--- components/ui/Icon/Loader.tsx
+++ components/ui/Icon/Loader.tsx
@@ -41,7 +41,6 @@ const LoaderContainer = styled.div<{ color: keyof Colors, size?: 'small' | 'medi
     position: absolute;
     width: ${({ size }) => size === 'small' ? '1.6rem' : size === 'medium' ? '2rem' : '6.4rem'};
     height: ${({ size }) => size === 'small' ? '1.6rem' : size === 'medium' ? '2rem' : '6.4rem'};
-    margin: 0.8rem;
     border: ${({ size }) => size === 'small' ? '0.2rem solid' : size === 'medium' ? '0.3rem solid' : '0.8rem solid'};
     border-radius: 50%;
     animation: ${rotation} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;

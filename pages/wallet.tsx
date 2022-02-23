cdb7d8b827e6e5b8c7de5866efdb407384d562c7
Igor Papandinas <igor.papandinas@posteo.net>
2022-02-23 11:03:20 +0100

    fix(wallet): getUser in SSR

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
+import cookies from 'next-cookies';
 
+import { getUser } from 'actions/user';
 import BetaBanner from 'components/base/BetaBanner';
 import FloatingHeader from 'components/base/FloatingHeader';
 import Footer from 'components/base/Footer';
@@ -8,7 +10,10 @@ import MainHeader from 'components/base/MainHeader';
 import Wallet from 'components/pages/Wallet';
 
 import { UserType } from 'interfaces';
+import { appSetUser } from 'redux/app';
 import { useMarketplaceData } from 'redux/hooks';
+import { wrapper } from 'redux/store';
+import { decryptCookie } from 'utils/cookie';
 
 export interface WalletPageProps {
   user: UserType;
@@ -32,5 +37,27 @@ const WalletPage = ({ user }: WalletPageProps) => {
     <FloatingHeader />
   </>
 }
+
+export const getServerSideProps = wrapper.getServerSideProps((store) => async (ctx) => {
+  const token = cookies(ctx).token && decryptCookie(cookies(ctx).token as string);
+  let user: UserType | null = null;
+
+  if (token) {
+    try {
+      user = await getUser(token, true);
+      store.dispatch(appSetUser(user));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (!user) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return { props: { user } };
+});
 
 export default WalletPage;
